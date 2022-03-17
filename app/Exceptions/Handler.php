975da5a292fab0c2860987919c38d52e5bec9b6a
Rafael Laurindo <rafaellaurindomf@icloud.com>
2022-03-17 16:16:08 -0300

    chore: handle validation exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +37,13 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (ValidationException $e) {
+            return response()->json([
+                'error' => 'Validation error',
+                'fields' => $e->validator->getMessageBag(),
+            ]);
         });
     }
 }
